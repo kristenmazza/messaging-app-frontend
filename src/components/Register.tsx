c29fdeccbe8833122b73d8a4a87c413548234837
kristenmazza <kbmazza@gmail.com>
2023-12-15 11:19:49 -0800

    Remove checkbox

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -299,14 +299,6 @@ export default function SignUp() {
                         Must match the first password input field.
                       </p>
                     </Grid>
-                    <Grid item xs={12}>
-                      <FormControlLabel
-                        control={
-                          <Checkbox value='allowExtraEmails' color='primary' />
-                        }
-                        label='I want to receive inspiration, marketing promotions and updates via email.'
-                      />
-                    </Grid>
                   </Grid>
 
                   <p
