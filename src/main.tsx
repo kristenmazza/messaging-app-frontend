794066779de9feb74ad0fd3a0eea633d60365fa5
kristenmazza <kbmazza@gmail.com>
2023-12-25 16:16:36 -0800

    Remove MSW implementation

--- src/main.tsx
+++ src/main.tsx
@@ -10,26 +10,14 @@ if (process.env.NODE_ENV === 'production') {
   disableReactDevTools();
 }
 
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
-
-  const { worker } = await import('./mocks/browser');
-
-  return worker.start();
-}
-
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route path='/*' element={<App />} />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </React.StrictMode>,
-  );
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
